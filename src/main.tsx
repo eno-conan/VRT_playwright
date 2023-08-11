6a638ce0beea60e1fda23f5b59068c37945a8531
eno-conan <eno49study@gmail.com>
2023-08-11 09:54:07 +0900

    install router and create pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import App from './App'
+import { RegisterUser } from './RegisterUser'
+import { MyPage } from './MyPage'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/register-user" element={<RegisterUser />} />
+        <Route path="/my-page" element={<MyPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
